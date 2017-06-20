114f53b88d2fad9737b3ee1f2fcd8c598a48d09a
Ludvig Böklin <ludvig.boklin@protonmail.com>
2017-06-20 19:18:31 +0200

    Make better

--- xmonad.nix
+++ xmonad.nix
@@ -19,6 +19,7 @@
           enableContribAndExtras = true;
           extraPackages = haskellPackages: with pkgs.haskellPackages; [
             X11
+            X11-xft
             containers
             directory
             xmobar
@@ -32,17 +33,40 @@
     };
 
     compton = {
+      package = pkgs.compton-git;
       enable = true;
       refreshRate = 144;
-      shadow = false;
       fade = true;
-      menuOpacity = "0.4";
-      shadowExclude = [
-        "window_type *= 'menu'"
-      ];
+      inactiveOpacity = "0.95";
+      menuOpacity = "0.8";
+      shadow = true;
+      shadowOffsets = [ (-8) (-8) ];
+      # shadowExclude = [
+      #   # "window_type *= 'menu'"
+      #   # Maybe reduntant with -C option:
+      #   "window_type *= 'dock'"
+      #   "window_type *= 'panel'"
+      # ];
+      extraOptions = ''
+        unredir-if-possible = true;
+        no-dock-shadow = true;
+        clear-shadow = true;
+        shadow-radius = 10;
+        detect-rounded-corners = true;
+        shadow-ignore-shaped = true;
+
+        # Window type settings
+        wintypes:
+        {
+          notify = { fade = true; shadow = true; opacity = 0.9; focus = true; };
+          tooltip = { fade = true; shadow = true; opacity = 0.9; focus = true; };
+        };
+      '';
     };
   };
-  environment.systemPackages = with pkgs.haskellPackages; [
-    xmobar
+  environment.systemPackages = with pkgs; [
+    # compton
+    rofi
+    haskellPackages.xmobar
   ];
 }
