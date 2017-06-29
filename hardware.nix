a9b1dc16007f69a7026f059a5e7f3d29d30d7a15
Ludvig Böklin <ludvig.boklin@protonmail.com>
2017-06-29 20:42:25 +0200

    Separate out peripherals from other hardware

--- hardware.nix
+++ hardware.nix
@@ -4,6 +4,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./peripherals.nix
   ];
 
   ## Storage devices ##
@@ -33,24 +34,5 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.extraDisplaySettings = ''
     Option         "metamodes" "DVI-D-0: 1920x1080_144 +0+0 {ForceCompositionPipeline=On}"
-  '';
-
-  ## Peripherals/Misc ##
-
-  hardware.bluetooth.enable = true;
-
-  # Steam Controller
-  # This allows SC-Controller application and daemon to access Steam Controller or its USB dongle.
-  # This is done by allowing read/write access to all users. You may want to change this to something like
-  # MODE="0660", GROUP="games" to allow r/w access only to members of that group.
-  services.udev.extraRules = ''
-    # This rule is needed for basic functionality of the controller in Steam and keyboard/mouse emulation
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
-    # This rule is necessary for gamepad emulation
-    KERNEL=="uinput", MODE="0660", GROUP="users", OPTIONS+="static_node=uinput"
-    # Valve HID devices over USB hidraw
-    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
-    # Valve HID devices over bluetooth hidraw
-    KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
   '';
 }
